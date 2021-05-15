c98fa1faf06bd3f561c4f61af5db2666e9e70a1c
ade_lana <lanadvictor@yahoo.co.uk>
2021-05-15 22:01:35 +0100

    installing more dpendencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    logger: "./src/logger.js"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    port: 9090,
+    compress: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development Management"
+    }),
+  ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
-  }
-}
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
