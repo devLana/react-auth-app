69bdb9adf39652f1f2fe673a395fc14b1d5aee0f
ade_lana <lanadvictor@yahoo.co.uk>
2021-05-20 17:47:48 +0100

    set up app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Landing from "./pages/Landing";
+import LogIn from "./pages/LogIn";
+import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
 
-const App = () => {
-  const [str, setStr] = useState("");
-  const [title] = useState("React made with Webpack & Babel");
-
-  useEffect(() => {
-    setStr(title);
-  }, [title]);
-
-  return <div>Hello {str}</div>;
-};
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Landing />
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard />
+      </Route>
+      <Route path="/signup">
+        <SignUp />
+      </Route>
+      <Route path="/login">
+        <LogIn />
+      </Route>
+      <Route exact path="/:profile">
+        <Profile />
+      </Route>
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
