fbbf2e91a4226b5a4b2253d3d7afb9079b8cbb19
ade_lana <lanadvictor@yahoo.co.uk>
2021-05-21 00:04:48 +0100

    set up app constants

---
+++ src/utils/Constants.js
@@ -0,0 +1,11 @@
+function Constants() {
+  this.LOGIN_ROUTE = "/login";
+  this.SIGN_UP_ROUTE = "/signup";
+  this.DASHBOARD_ROUTE = "/dashboard";
+  this.LANDING_ROUTE = "/";
+  this.PROFILE_ROUTE = "/:profile";
+}
+
+const constants = new Constants();
+
+export default constants;
