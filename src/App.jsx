aab9a8d7abe28df3460ee04b67ab522785b7ac34
ade_lana <lanadvictor@yahoo.co.uk>
2021-05-21 00:30:11 +0100

    Authenticate app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AuthenticateRoutes from "./Components/ProtectedRoutes/AuthenticateRoutes";
+import AuthenticateLanding from "./Components/ProtectedRoutes/AuthenticateLanding";
 import Dashboard from "./pages/Dashboard";
-import Landing from "./pages/Landing";
 import LogIn from "./pages/LogIn";
 import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
+import constants from "./utils/Constants";
 
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/">
-        <Landing />
-      </Route>
-      <Route path="/dashboard">
-        <Dashboard />
-      </Route>
-      <Route path="/signup">
+      <AuthenticateLanding path={constants.LANDING_ROUTE} exact />
+      <AuthenticateRoutes
+        component={Dashboard}
+        path={constants.DASHBOARD_ROUTE}
+        exact
+      />
+      <Route path={constants.SIGN_UP_ROUTE} exact>
         <SignUp />
       </Route>
-      <Route path="/login">
+      <Route path={constants.LOGIN_ROUTE} exact>
         <LogIn />
       </Route>
-      <Route exact path="/:profile">
-        <Profile />
-      </Route>
+      <AuthenticateRoutes
+        component={Profile}
+        path={constants.PROFILE_ROUTE}
+        exact
+      />
       <Route>
         <NotFound />
       </Route>
