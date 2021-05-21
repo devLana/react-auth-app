048bf4801d57e407a94736d81c5ac56c0a537701
ade_lana <lanadvictor@yahoo.co.uk>
2021-05-21 23:38:32 +0100

    complete Form jsx

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,47 +1,98 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 
-const formFields = { userName: "", password: "" };
+const formFields = { username: "", password: "" };
 
-const Form = ({ type }) => {
+const Form = ({ type, submitHandler }) => {
   const [values, setValues] = useState(formFields);
   const [errors, setErrors] = useState(formFields);
+  const [visible, setVisible] = useState(false);
 
-  const submitHandler = {};
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleFocus = e => {
+    const { name, parentElement } = e.target;
+
+    parentElement.classList.remove("form-error");
+    setErrors({ ...errors, [name]: "" });
+  };
+
+  const handleBlur = e => {
+    const { name, parentElement } = e.target;
+
+    if (errors[name]) {
+      parentElement.classList.add("form-error");
+    }
+  };
+
+  const handleVisibility = e => {
+    setVisible(e.target.checked);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    submitHandler(values, setErrors);
+  };
 
   return (
-    <form noValidate onSubmit={submitHandler}>
+    <form noValidate onSubmit={handleSubmit}>
       <div className="form__group">
         <div className="form__input">
-          <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            name="userName"
-            value={values.userName}
-          />
+          <label className="form__label" htmlFor="username">
+            <span className="form__input--label">Username</span>
+            <input
+              id="username"
+              type="text"
+              name="username"
+              onChange={handleChange}
+              value={values.username}
+            />
+          </label>
         </div>
-        {errors.userName && (
-          <div className="form__error">{errors.userName}</div>
+        {errors.username && (
+          <span className="form__input--error">{errors.username}</span>
         )}
       </div>
       <div className="form__group">
         <div className="form__input">
-          <label htmlFor="password">Username</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            value={values.password}
-          />
+          <label className="form__label" htmlFor="password">
+            <span className="form__input--label">Password</span>
+            <input
+              id="password"
+              type={visible ? "text" : "password"}
+              name="password"
+              onChange={handleChange}
+              value={values.password}
+            />
+          </label>
         </div>
         {errors.password && (
-          <div className="form__error">{errors.password}</div>
+          <span className="form__input--error">{errors.password}</span>
         )}
       </div>
       <div className="form__group">
         <div className="form__input">
+          <label htmlFor="checkbox" className="form__input">
+            <span className="form__input--label">
+              {visible ? "Hide" : "Show"} Password
+            </span>
+            <input
+              id="checkbox"
+              type="checkbox"
+              checked={visible}
+              onChange={handleVisibility}
+            />
+          </label>
+        </div>
+      </div>
+      <div className="form__group">
+        <div className="form__input">
           <input
+            id="submit__btn"
             type="submit"
             value={type === "login" ? "Log in" : "Sign up"}
           />
@@ -53,6 +104,7 @@ const Form = ({ type }) => {
 
 Form.propTypes = {
   type: PropTypes.string,
+  submitHandler: PropTypes.func.isRequired,
 };
 
 Form.defaultProps = {
