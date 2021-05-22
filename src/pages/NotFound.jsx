a44372ce9622c8cd2c16858e3655f4c662009062
ade_lana <lanadvictor@yahoo.co.uk>
2021-05-22 19:49:08 +0100

    Not Found Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,25 @@
-const NotFound = () => <div>Not Found</div>;
+import { Link } from "react-router-dom";
+import useDocTitle from "../hooks/useDocTitle";
+import constants from "../utils/Constants";
+
+const NotFound = () => {
+  useDocTitle("Not Found");
+
+  return (
+    <main>
+      <header>
+        <h1>Not Found</h1>
+      </header>
+      <section>
+        <div>
+          <p>The content you are looking for does not exist</p>
+        </div>
+        <div>
+          <Link to={constants.LANDING_ROUTE}>Go home</Link>
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default NotFound;
