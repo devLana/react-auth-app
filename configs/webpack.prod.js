5f81ac896334c5a7aa0c00da4fb1efbe54332512
ade_lana <lanadvictor@yahoo.co.uk>
2021-05-24 19:32:31 +0100

    ...

--- configs/webpack.prod.js
+++ configs/webpack.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: "production",
   output: {
     // filename: "app-[contenthash].js",
-    filename: "../build/js/app-[contenthash].js",
+    filename: "js/app-[contenthash].js",
     publicPath: "/",
   },
   devtool: "source-map",
@@ -21,7 +21,7 @@ module.exports = merge(common, {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "../build/css/index.[contenthash].css",
+      filename: "css/index.[contenthash].css",
     }),
   ],
   optimization: {
