9ccbfa442a35cd129acc7991434a0d64c38ef258
ade_lana <lanadvictor@yahoo.co.uk>
2021-05-24 22:20:14 +0100

    implement screen width custom hook

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,40 +1,47 @@
 import { Link } from "react-router-dom";
 import constants from "../utils/Constants";
+import useScreenWidth from "../hooks/useScreenWidth";
 import Avatar from "./Avatar";
 import Image from "./Image";
 
-const Navbar = () => (
-  <nav>
-    <div>
-      <Link to={constants.LANDING_ROUTE}>
-        <Image />
-      </Link>
-    </div>
-    <div>
-      <Link to={constants.DASHBOARD_ROUTE}>Dashboard</Link>
-    </div>
-    <div>
-      <Avatar />
-    </div>
-    <div>
-      <ol>
-        <li>
-          <div>
-            <Avatar />
-          </div>
-        </li>
-        <li>
-          <span>View Profile</span>
-        </li>
-        <li>
+const Navbar = () => {
+  const screenWidth = useScreenWidth();
+
+  return (
+    <nav>
+      <div>
+        <Link to={constants.LANDING_ROUTE}>
+          <Image />
+        </Link>
+      </div>
+      {screenWidth > 500 && (
+        <div>
           <Link to={constants.DASHBOARD_ROUTE}>Dashboard</Link>
-        </li>
-        <li>
-          <span>Logout</span>
-        </li>
-      </ol>
-    </div>
-  </nav>
-);
+        </div>
+      )}
+      <div>
+        <Avatar />
+      </div>
+      <div>
+        <ol>
+          <li>
+            <div>
+              <Avatar />
+            </div>
+          </li>
+          <li>
+            <Link to={constants.PROFILE_ROUTE}>View Profile</Link>
+          </li>
+          <li>
+            <Link to={constants.DASHBOARD_ROUTE}>Dashboard</Link>
+          </li>
+          <li>
+            <span>Logout</span>
+          </li>
+        </ol>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
