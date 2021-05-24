5a325d67705fab878f98a6386a32ccfcffd3f77f
ade_lana <lanadvictor@yahoo.co.uk>
2021-05-24 19:04:45 +0100

    setup Navbar links

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,40 @@
-const Navbar = () => {
-  return <div>Page Navbar</div>;
-};
+import { Link } from "react-router-dom";
+import constants from "../utils/Constants";
+import Avatar from "./Avatar";
+import Image from "./Image";
+
+const Navbar = () => (
+  <nav>
+    <div>
+      <Link to={constants.LANDING_ROUTE}>
+        <Image />
+      </Link>
+    </div>
+    <div>
+      <Link to={constants.DASHBOARD_ROUTE}>Dashboard</Link>
+    </div>
+    <div>
+      <Avatar />
+    </div>
+    <div>
+      <ol>
+        <li>
+          <div>
+            <Avatar />
+          </div>
+        </li>
+        <li>
+          <span>View Profile</span>
+        </li>
+        <li>
+          <Link to={constants.DASHBOARD_ROUTE}>Dashboard</Link>
+        </li>
+        <li>
+          <span>Logout</span>
+        </li>
+      </ol>
+    </div>
+  </nav>
+);
 
 export default Navbar;
