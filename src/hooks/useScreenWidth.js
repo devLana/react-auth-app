a26a37f5ee43362e3e504d8c4d9fb317ae9a1637
ade_lana <lanadvictor@yahoo.co.uk>
2021-05-24 22:19:40 +0100

    screen width custom hook

---
+++ src/hooks/useScreenWidth.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const useScreenWidth = () => {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const updateWidth = () => {
+      setWidth(window.screen.width);
+    };
+
+    window.addEventListener("resize", updateWidth);
+
+    // updateWidth();
+
+    return () => {
+      window.removeEventListener("resize", updateWidth);
+    };
+  }, []);
+
+  return width;
+};
+
+export default useScreenWidth;
