ab11dcc44ede019a8d066c42947b81cea16e63d8
ade_lana <lanadvictor@yahoo.co.uk>
2021-05-31 20:40:45 +0100

    remove unused variable from webpack config

--- configs/webpack.dev.js
+++ configs/webpack.dev.js
@@ -2,7 +2,6 @@ const { join } = require("path");
 const webpack = require("webpack");
 const { merge } = require("webpack-merge");
 const ESLintPlugin = require("eslint-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
