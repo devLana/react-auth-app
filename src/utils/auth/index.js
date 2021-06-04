57db0e3f14760668383847c3ca8a5784bb8cb7c7
ade_lana <lanadvictor@yahoo.co.uk>
2021-06-04 22:54:39 +0100

    modified auth.js for testing

--- src/utils/auth/index.js
+++ src/utils/auth/index.js
@@ -1,23 +1,37 @@
-export const getUser = () => {
-  const user = localStorage.getItem("user");
+export const getUser = (env = "prod") => {
+  let user;
+
+  if (env === "prod") {
+    user = window.localStorage.getItem("user");
+  } else {
+    user = window.localStorage.getItem("test-user");
+  }
 
   return user ? JSON.parse(user) : null;
 };
 
-export const setUser = user => {
+export const setUser = (user, env = "prod") => {
   const saveUser = JSON.stringify(user);
 
-  localStorage.setItem("user", saveUser);
+  if (env === "prod") {
+    window.localStorage.setItem("user", saveUser);
+  } else {
+    window.localStorage.setItem("test-user", saveUser);
+  }
 };
 
-export const isLoggedIn = () => {
-  const user = getUser();
+export const isLoggedIn = (env = "prod") => {
+  const user = getUser(env);
 
   return !!user;
 };
 
-export const logOut = cb => {
-  localStorage.removeItem("user");
+export const logOut = (cb, env = "prod") => {
+  if (env === "prod") {
+    window.localStorage.removeItem("user");
+  } else {
+    window.localStorage.removeItem("test-user");
+  }
 
   cb();
 };
