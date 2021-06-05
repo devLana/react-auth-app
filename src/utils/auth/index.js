9dab13b66f4506d84a00c5b27784531f74a1fe5c
ade_lana <lanadvictor@yahoo.co.uk>
2021-06-05 20:17:51 +0100

    jest config file

--- src/utils/auth/index.js
+++ src/utils/auth/index.js
@@ -2,9 +2,9 @@ export const getUser = (env = "prod") => {
   let user;
 
   if (env === "prod") {
-    user = window.localStorage.getItem("user");
+    user = localStorage.getItem("user");
   } else {
-    user = window.localStorage.getItem("test-user");
+    user = localStorage.getItem("test-user");
   }
 
   return user ? JSON.parse(user) : null;
@@ -14,9 +14,9 @@ export const setUser = (user, env = "prod") => {
   const saveUser = JSON.stringify(user);
 
   if (env === "prod") {
-    window.localStorage.setItem("user", saveUser);
+    localStorage.setItem("user", saveUser);
   } else {
-    window.localStorage.setItem("test-user", saveUser);
+    localStorage.setItem("test-user", saveUser);
   }
 };
 
@@ -28,9 +28,9 @@ export const isLoggedIn = (env = "prod") => {
 
 export const logOut = (cb, env = "prod") => {
   if (env === "prod") {
-    window.localStorage.removeItem("user");
+    localStorage.removeItem("user");
   } else {
-    window.localStorage.removeItem("test-user");
+    localStorage.removeItem("test-user");
   }
 
   cb();
