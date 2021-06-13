90fbfa9dd7c4818dabf6acca896c13110b3bed27
ade_lana <lanadvictor@yahoo.co.uk>
2021-06-13 04:50:09 +0100

    client changes

--- src/client/index.js
+++ src/client/index.js
@@ -3,12 +3,12 @@ import "core-js/modules/es.promise";
 import "core-js/modules/es.array.iterator";
 
 import { StrictMode } from "react";
-import { render } from "react-dom";
+import { hydrate } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "../app/App";
 
-render(
+hydrate(
   <StrictMode>
     <BrowserRouter>
       <App />
