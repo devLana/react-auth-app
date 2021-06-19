63be8fac10b3cd53e6492686a77940bfa65bd95f
ade_lana <lanadvictor@yahoo.co.uk>
2021-06-19 23:15:26 +0100

    webpack server changes

--- configs/webpack.server.js
+++ configs/webpack.server.js
@@ -1,72 +1,57 @@
 const { join, resolve } = require("path");
-const webpack = require("webpack");
+// const webpack = require("webpack");
 const nodeExternals = require("webpack-node-externals");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  target: "node",
-  mode: "production",
-  externals: [nodeExternals()],
-  entry: {
-    server: join(process.cwd(), "src", "server", "index.js"),
-  },
-  output: {
-    path: resolve(process.cwd(), "server-build"),
-    clean: true,
-    publicPath: "/",
-    filename: "js/[name].bundle.js",
-    assetModuleFilename: "images/[ext][query]",
-  },
-  devServer: {
-    contentBase: "../server-build",
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "css/styles.css",
-    }),
-    new webpack.ProvidePlugin({
-      React: "react",
-      ReactDOM: "react-dom",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/i,
-        use: ["babel-loader"],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(jpg|jpeg|png)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
-      },
-    ],
-  },
-  // optimization: {
-  //   splitChunks: {
-  //     cacheGroups: {
-  //       commons: {
-  //         test: /[\\/]node_modules[\\/]/,
-  //         name: "commons",
-  //         chunks: "all",
-  //       },
-  //     },
-  //   },
-  //   runtimeChunk: "single",
-  // },
-  optimization: {
-    runtimeChunk: "single",
-  },
-  resolve: {
-    extensions: [".jsx", "..."],
-  },
+module.exports = env => {
+  return {
+    mode: env.production ? "production" : "development",
+    devtool: env.production ? "source-map" : "inline-source-map",
+    target: "node",
+    externals: [nodeExternals()],
+    entry: {
+      server: join(process.cwd(), "src", "server", "index.js"),
+    },
+    output: {
+      path: resolve(process.cwd(), "server-build"),
+      clean: true,
+      // publicPath: "/",
+      filename: "js/[name].bundle.js",
+      // assetModuleFilename: "images/[ext][query]",
+    },
+    resolve: {
+      extensions: [".jsx", "..."],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/i,
+          use: ["babel-loader"],
+          exclude: /node_modules/,
+        },
+        // {
+        //   test: /\.(jpg|jpeg|png)$/i,
+        //   type: "asset/resource",
+        // },
+        // {
+        //   test: /\.s[ac]ss$/i,
+        //   use: [
+        //     MiniCssExtractPlugin.loader,
+        //     "css-loader",
+        //     "postcss-loader",
+        //     "sass-loader",
+        //   ],
+        // },
+      ],
+    },
+    // plugins: [
+    //   new MiniCssExtractPlugin({
+    //     filename: "css/styles.css",
+    //   }),
+    //   new webpack.ProvidePlugin({
+    //     React: "react",
+    //     ReactDOM: "react-dom",
+    //   }),
+    // ],
+  };
 };
