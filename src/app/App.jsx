71121ed9373a8bf8d83a3294258b2fe07f7927f0
ade_lana <lanadvictor@yahoo.co.uk>
2021-06-29 18:34:23 +0100

    remove client side db object

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useState, lazy } from "react";
+import React, { Suspense, lazy } from "react";
 import { Switch, Route } from "react-router-dom";
 import AuthenticateRoutes from "./Components/ProtectedRoutes/AuthenticateRoutes";
 import AuthenticateLanding from "./Components/ProtectedRoutes/AuthenticateLanding";
@@ -6,8 +6,6 @@ import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import SignUp from "./pages/SignUp";
 import constants from "./utils/Constants";
-import usersDB from "./utils/usersDB";
-import { getUser } from "./utils/auth";
 import "./index.scss";
 
 const Dashboard = lazy(() => {
@@ -18,49 +16,33 @@ const Profile = lazy(() => {
   return import(/* webpackChunkName: "Profile" */ "./pages/Profile");
 });
 
-const App = () => {
-  const [users, setUsers] = useState(usersDB);
-
-  useEffect(() => {
-    const user = getUser();
-
-    if (user) {
-      setUsers(state => [...state, user]);
-    }
-  }, []);
-
-  const handleUsers = user => {
-    setUsers([...users, user]);
-  };
-
-  return (
-    <>
-      <Suspense fallback={<div className="loader__text">Loading...</div>}>
-        <Switch>
-          <AuthenticateLanding path={constants.LANDING_ROUTE} exact />
-          <AuthenticateRoutes
-            component={Dashboard}
-            path={constants.DASHBOARD_ROUTE}
-            exact
-          />
-          <Route path={constants.SIGN_UP_ROUTE} exact>
-            <SignUp users={users} handleUsers={handleUsers} />
-          </Route>
-          <Route path={constants.LOGIN_ROUTE} exact>
-            <Login users={users} />
-          </Route>
-          <AuthenticateRoutes
-            component={Profile}
-            path={constants.PROFILE_ROUTE}
-            exact
-          />
-          <Route>
-            <NotFound />
-          </Route>
-        </Switch>
-      </Suspense>
-    </>
-  );
-};
+const App = () => (
+  <>
+    <Suspense fallback={<div className="loader__text">Loading...</div>}>
+      <Switch>
+        <AuthenticateLanding path={constants.LANDING_ROUTE} exact />
+        <AuthenticateRoutes
+          component={Dashboard}
+          path={constants.DASHBOARD_ROUTE}
+          exact
+        />
+        <Route path={constants.SIGN_UP_ROUTE} exact>
+          <SignUp />
+        </Route>
+        <Route path={constants.LOGIN_ROUTE} exact>
+          <Login />
+        </Route>
+        <AuthenticateRoutes
+          component={Profile}
+          path={constants.PROFILE_ROUTE}
+          exact
+        />
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Suspense>
+  </>
+);
 
 export default App;
--- src/app/utils/usersDB.js
+++
@@ -1,6 +0,0 @@
-const usersDB = [
-  { username: "jack", password: "1234abc" },
-  { username: "james105", password: "qwerty08" },
-];
-
-export default usersDB;
